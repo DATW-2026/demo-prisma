8dc3ebeeb3a18864f10d5a4bd2e726db61b7ba7b
Yevestevez <yevestevez.cg@gmail.com>
2026-04-24 11:43:42 +0200

    Implement ReviewsRepo class for managing film reviews with CRUD operations

---
+++ src/repos/reviews-repo.ts
@@ -0,0 +1,91 @@
+import type { PrismaClient } from '../../generated/prisma/client.ts';
+import type {
+    ReviewCreateInput,
+    ReviewUpdateInput,
+} from '../../generated/prisma/models.ts';
+
+import { env } from '../config/env.ts';
+import debug from 'debug';
+
+const log = debug(`${env.PROJECT_NAME}:repo:reviews`);
+log('Loading reviews repo...');
+
+export class ReviewsRepo {
+    #prisma: PrismaClient;
+
+    constructor(prisma: PrismaClient) {
+        this.#prisma = prisma;
+    }
+
+    getUserReviews = async (userId: number) => {
+        const result = await this.#prisma.review.findMany({
+            where: {
+                userId: userId,
+            },
+        });
+
+        // TODO -> Que no haya reviews no es un error
+        if (result.length === 0) {
+            throw new Error('Reviews Not Found');
+        }
+
+        return result;
+    };
+
+    getFilmReviews = async (filmId: number) => {
+        const result = await this.#prisma.review.findMany({
+            where: {
+                filmId: filmId,
+            },
+        });
+
+        // TODO -> Que no haya reviews no es un error
+        if (result.length === 0) {
+            throw new Error('Reviews Not Found');
+        }
+
+        return result;
+    };
+
+    // TODO -> PERMISOS SOLO PARA USER
+    createReview = async (reviewData: ReviewCreateInput) => {
+        const result = await this.#prisma.review.create({
+            data: reviewData,
+        });
+
+        return result;
+    };
+
+    // TODO -> PERMISOS SOLO PARA OWNER
+    updateReview = async (
+        userId: number,
+        filmId: number,
+        reviewData: ReviewUpdateInput,
+    ) => {
+        const result = await this.#prisma.review.update({
+            where: {
+                userId_filmId: {
+                    userId: userId,
+                    filmId: filmId,
+                },
+            },
+            data: reviewData,
+        });
+
+        return result;
+    };
+
+    // TODO-> PERMISOS SOLO PARA OWNER Y ADMIN
+    deleteReview = async (userId: number, filmId: number) => {
+        const result = await this.#prisma.review.delete({
+            where: {
+                userId_filmId: {
+                    userId: userId,
+                    filmId: filmId,
+                },
+            },
+        });
+
+        return result;
+    };
+}
