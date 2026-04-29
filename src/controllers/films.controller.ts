6313b6e3352c7488a3ee40436edbd133802e8f83
Eduardo Yeves <yevestevez.cg@gmail.com>
2026-04-29 11:07:21 +0100

    Implement FilmsController with CRUD operations and error handling

---
+++ src/controllers/films.controller.ts
@@ -0,0 +1,154 @@
+import debug from 'debug';
+import type { NextFunction, Request, Response } from 'express';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client';
+
+import { env } from '../config/env.ts';
+import { HttpError } from '../errors/http-error.ts';
+import type { FilmsRepo } from '../repos/films.repo.ts';
+import type { Film, FilmUpdateDTO } from '../zod/film.schemas.ts';
+
+const log = debug(`${env.PROJECT_NAME}:controller:films`);
+log('Starting films controller...');
+
+export class FilmsController {
+    #repo: FilmsRepo;
+
+    constructor(repo: FilmsRepo) {
+        this.#repo = repo;
+    }
+
+    getAllFilms = async (req: Request, res: Response, next: NextFunction) => {
+        log('Getting all films...');
+
+        try {
+            const films: Film[] = await this.#repo.getAllFilms();
+
+            res.json(films);
+        } catch (error) {
+            const finalError = new HttpError(
+                500,
+                'Internal Server Error',
+                'Failed to get films',
+                {
+                    cause: error,
+                },
+            );
+
+            return next(finalError);
+        }
+    };
+
+    getFilmById = async (req: Request, res: Response, next: NextFunction) => {
+        const id = Number(req.params.id);
+        log(`Getting Film with id ${id} from repo...`);
+
+        try {
+            const film: Film = await this.#repo.getFilmById(id);
+
+            res.json(film);
+        } catch (error) {
+            log('Error getting film by id: %O', error);
+            if (error instanceof PrismaClientKnownRequestError) {
+                const finalError = new HttpError(
+                    404,
+                    'Not Found',
+                    `Film with id ${id} not found`,
+                    {
+                        cause: error,
+                    },
+                );
+
+                return next(finalError);
+            }
+
+            const finalError = new HttpError(
+                500,
+                'Internal Server Error',
+                'Failed to get film',
+                {
+                    cause: error,
+                },
+            );
+
+            return next(finalError);
+        }
+    };
+
+    // createFilm = ...
+
+    updateFilm = async (req: Request, res: Response, next: NextFunction) => {
+        const id = Number(req.params.id);
+        log(`Updating Film with id ${id} in repo...`);
+
+        try {
+            const filmData: FilmUpdateDTO = req.body;
+            const updatedFilm: Film = await this.#repo.updateFilmById(
+                id,
+                filmData,
+            );
+
+            res.json(updatedFilm);
+        } catch (error) {
+            log('Error updating film by id: %O', error);
+            if (error instanceof PrismaClientKnownRequestError) {
+                const finalError = new HttpError(
+                    404,
+                    'Not Found',
+                    `Film with id ${id} not found`,
+                    {
+                        cause: error,
+                    },
+                );
+
+                return next(finalError);
+            }
+
+            const finalError = new HttpError(
+                500,
+                'Internal Server Error',
+                'Failed to update film',
+                {
+                    cause: error,
+                },
+            );
+
+            return next(finalError);
+        }
+    };
+
+    deleteFilm = async (req: Request, res: Response, next: NextFunction) => {
+        const id = Number(req.params.id);
+        log(`Deleting Film with id ${id} from repo...`);
+
+        try {
+            const deletedfilm: Film = await this.#repo.deleteFilmById(id);
+
+            res.json(deletedfilm);
+        } catch (error) {
+            log('Error deleting film by id: %O', error);
+            if (error instanceof PrismaClientKnownRequestError) {
+                const finalError = new HttpError(
+                    404,
+                    'Not Found',
+                    `Film with id ${id} not found`,
+                    {
+                        cause: error,
+                    },
+                );
+
+                return next(finalError);
+            }
+
+            const finalError = new HttpError(
+                500,
+                'Internal Server Error',
+                'Failed to delete film',
+                {
+                    cause: error,
+                },
+            );
+
+            return next(finalError);
+        }
+    };
+}
